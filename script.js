8b0b32f2e7f02155c9882e05d25132cc3fb50dab
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-03-02 23:20:10 -0800

    reformatted the name of some methods in snake_case for consistency.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var click_event_happening = null;
 
 $(document).ready(function () {
     apps_array = [];
-    drawTriangles();
+    draw_triangles();
     load_files();
     apply_standard_event_handlers();
 
@@ -250,160 +250,160 @@ function jump_to_app(new_app_index){
 
 
 //draw name in triangles
-function drawTriangles(){
+function draw_triangles(){
   //draw k
-  drawPoly(1,6);
-  drawPoly(1,7);
-  drawPoly(2,5);
-  drawPoly(2,6);
-  drawPoly(3,4);
-  drawPoly(3,5);
-  drawPoly(4,3);
-  drawPoly(4,4);
-  drawPoly(4,5);
-  drawPoly(4,6);
-  drawPoly(4,7);
-  drawPoly(4,8);
-  drawPoly(5,2);
-  drawPoly(5,3);
-  drawPoly(5,5);
-  drawPoly(5,6);
-  drawPoly(6,1);
-  drawPoly(6,2);
-  drawPoly(6,6);
-  drawPoly(6,7);
+  draw_poly(1,6);
+  draw_poly(1,7);
+  draw_poly(2,5);
+  draw_poly(2,6);
+  draw_poly(3,4);
+  draw_poly(3,5);
+  draw_poly(4,3);
+  draw_poly(4,4);
+  draw_poly(4,5);
+  draw_poly(4,6);
+  draw_poly(4,7);
+  draw_poly(4,8);
+  draw_poly(5,2);
+  draw_poly(5,3);
+  draw_poly(5,5);
+  draw_poly(5,6);
+  draw_poly(6,1);
+  draw_poly(6,2);
+  draw_poly(6,6);
+  draw_poly(6,7);
 
   //draw y
-  drawPoly(4,12);
-  drawPoly(4,13);
-  drawPoly(5,13);
-  drawPoly(5,14);
-  drawPoly(6,14);
-  drawPoly(4,17);
-  drawPoly(4,18);
-  drawPoly(5,16);
-  drawPoly(5,17);
-  drawPoly(6,15);
-  drawPoly(6,16);
-  drawPoly(7,14);
-  drawPoly(7,15);
-  drawPoly(8,13);
-  drawPoly(8,14);
-  drawPoly(9,12);
-  drawPoly(9,13);
+  draw_poly(4,12);
+  draw_poly(4,13);
+  draw_poly(5,13);
+  draw_poly(5,14);
+  draw_poly(6,14);
+  draw_poly(4,17);
+  draw_poly(4,18);
+  draw_poly(5,16);
+  draw_poly(5,17);
+  draw_poly(6,15);
+  draw_poly(6,16);
+  draw_poly(7,14);
+  draw_poly(7,15);
+  draw_poly(8,13);
+  draw_poly(8,14);
+  draw_poly(9,12);
+  draw_poly(9,13);
     
   //draw l
-  drawPoly(6,19);
-  drawPoly(6,20);
-  drawPoly(5,20);
-  drawPoly(5,21);
-  drawPoly(4,21);
-  drawPoly(4,22);
-  drawPoly(3,22);
-  drawPoly(3,23);
-  drawPoly(2,23);
-  drawPoly(2,24);
-  drawPoly(1,24);
-  drawPoly(1,25);
+  draw_poly(6,19);
+  draw_poly(6,20);
+  draw_poly(5,20);
+  draw_poly(5,21);
+  draw_poly(4,21);
+  draw_poly(4,22);
+  draw_poly(3,22);
+  draw_poly(3,23);
+  draw_poly(2,23);
+  draw_poly(2,24);
+  draw_poly(1,24);
+  draw_poly(1,25);
 
   //draw e
-  drawPoly(6,26);
-  drawPoly(6,27);
-  drawPoly(5,25);
-  drawPoly(5,26);
-  drawPoly(4,25);
-  drawPoly(4,26);
-  drawPoly(4,27);
-  drawPoly(3,26);
-  drawPoly(3,27);
-  drawPoly(3,28);
-  drawPoly(3,29);
-  drawPoly(3,30);
-  drawPoly(4,30);
-  drawPoly(4,29);
-  drawPoly(5,29);
+  draw_poly(6,26);
+  draw_poly(6,27);
+  draw_poly(5,25);
+  draw_poly(5,26);
+  draw_poly(4,25);
+  draw_poly(4,26);
+  draw_poly(4,27);
+  draw_poly(3,26);
+  draw_poly(3,27);
+  draw_poly(3,28);
+  draw_poly(3,29);
+  draw_poly(3,30);
+  draw_poly(4,30);
+  draw_poly(4,29);
+  draw_poly(5,29);
 
   //draw m
-  drawPoly(6,35);
-  drawPoly(6,36);
-  drawPoly(5,36);
-  drawPoly(5,37);
-  drawPoly(4,37);
-  drawPoly(4,38);
-  drawPoly(3,38);
-  drawPoly(4,39);
-  drawPoly(4,40);
-  drawPoly(4,41);
-  drawPoly(5,41);
-  drawPoly(5,40);
-  drawPoly(6,40);
-  drawPoly(6,39);
-  drawPoly(4,43);
-  drawPoly(4,44);
-  drawPoly(4,45);
-  drawPoly(5,45);
-  drawPoly(5,44);
-  drawPoly(6,44);
-  drawPoly(6,43);
+  draw_poly(6,35);
+  draw_poly(6,36);
+  draw_poly(5,36);
+  draw_poly(5,37);
+  draw_poly(4,37);
+  draw_poly(4,38);
+  draw_poly(3,38);
+  draw_poly(4,39);
+  draw_poly(4,40);
+  draw_poly(4,41);
+  draw_poly(5,41);
+  draw_poly(5,40);
+  draw_poly(6,40);
+  draw_poly(6,39);
+  draw_poly(4,43);
+  draw_poly(4,44);
+  draw_poly(4,45);
+  draw_poly(5,45);
+  draw_poly(5,44);
+  draw_poly(6,44);
+  draw_poly(6,43);
 
   //draw a
-  drawPoly(6,48);
-  drawPoly(6,49);
-  drawPoly(6,50);
-  drawPoly(6,52);
-  drawPoly(5,48);
-  drawPoly(5,49);
-  drawPoly(5,51);
-  drawPoly(5,52);
-  drawPoly(4,49);
-  drawPoly(4,50);
-  drawPoly(4,51);
+  draw_poly(6,48);
+  draw_poly(6,49);
+  draw_poly(6,50);
+  draw_poly(6,52);
+  draw_poly(5,48);
+  draw_poly(5,49);
+  draw_poly(5,51);
+  draw_poly(5,52);
+  draw_poly(4,49);
+  draw_poly(4,50);
+  draw_poly(4,51);
 
   //draw r
-  drawPoly(6,55);
-  drawPoly(6,56);
-  drawPoly(5,56);
-  drawPoly(5,57);
-  drawPoly(4,57);
-  drawPoly(4,58);
-  drawPoly(4,59);
-  drawPoly(4,60);
+  draw_poly(6,55);
+  draw_poly(6,56);
+  draw_poly(5,56);
+  draw_poly(5,57);
+  draw_poly(4,57);
+  draw_poly(4,58);
+  draw_poly(4,59);
+  draw_poly(4,60);
 
   //draw x
-  drawPoly(3,63);
-  drawPoly(3,64);
-  drawPoly(3,66);
-  drawPoly(3,67);
-  drawPoly(4,64);
-  drawPoly(4,65);
-  drawPoly(4,66);
-  drawPoly(5,64);
-  drawPoly(5,65);
-  drawPoly(5,66);
-  drawPoly(6,63);
-  drawPoly(6,64);
-  drawPoly(6,66);
-  drawPoly(6,67);
+  draw_poly(3,63);
+  draw_poly(3,64);
+  draw_poly(3,66);
+  draw_poly(3,67);
+  draw_poly(4,64);
+  draw_poly(4,65);
+  draw_poly(4,66);
+  draw_poly(5,64);
+  draw_poly(5,65);
+  draw_poly(5,66);
+  draw_poly(6,63);
+  draw_poly(6,64);
+  draw_poly(6,66);
+  draw_poly(6,67);
 
   //scale the name drawn
-  scaleDown();
+  scale_down();
 }
 
-function scaleDown(){
+function scale_down(){
   $('#main').attr('transform', "scale(0.125)");
 }
 
-function drawPoly(row, column) {
+function draw_poly(row, column) {
     var poly = document.createElementNS("http://www.w3.org/2000/svg","polygon");
     poly.setAttribute("class", 'name');
-    points =  getPolyPoints(row, column);
+    points =  get_poly_points(row, column);
     poly.setAttribute("points", points);
   
     // $('#canvas').append(poly);
     $('#main').append(poly);
 }
 
-function getPolyPoints(row, column){
+function get_poly_points(row, column){
   var x=[];
   var y=[];
   var retStr = "";
