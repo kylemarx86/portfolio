33581bd7a2ec968b2b7fe41bd74276b491cf2035
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-07-20 17:53:45 -0700

    clean up mail_handler. change altBody in mail handler.

--- mailer/mail_handler.php
+++ mailer/mail_handler.php
@@ -27,18 +27,10 @@ $mail->smtpConnect($options);
 $mail->From = EMAIL_USER; //your email sending account
 $mail->FromName = 'Kyle Marx';//your email sending account name
 
-// $mail->addAddress(your email address, or the email the sender if you are sending confirmation, email address user name);     // Add a recipient   // Name is optional
 $mail->addAddress(EMAIL_USER, 'Kyle Marx');     // Add a recipient
-// $mail->addAddress($_POST['email'], $_POST['name']);     // Add a recipient
 
-// $mail->addReplyTo(/*email address of the person sending the message, so you can reply*/);
 $mail->addReplyTo($_POST['email'], $_POST['name']);
 
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
 // construct message
@@ -47,9 +39,9 @@ $email = $_POST['email'];
 $body = $_POST['body'];
 $message = "<p>name: $name</p><p>email: $email</p>$body";
 
-$mail->Subject = $_POST['subject'];         //accept a post variable
-$mail->Body = $message;         //accept a post variable       //not anymore
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';        //i should change this
+$mail->Subject = $_POST['subject'];         // set message subject
+$mail->Body = $message;                 // set message body
+$mail->AltBody = strip_tags($message);      // set alternate text for non-html clients
 
 $output = [];
 if(!$mail->send()) {
